c7afb694d85c60984861cd26ec99e90fba9ed6fd
maryamf93 <maryamfooladi21@yahoo.com>
2019-06-19 00:58:53 +0430

    use react-select & handleChange function

--- my-task/src/App.js
+++ my-task/src/App.js
@@ -5,16 +5,77 @@ import CaseThird from './Components/CaseThird'
 import CaseFourth from './Components/CaseFourth'
 import CaseFifth from './Components/CaseFifth'
 import './App.css'
+import Select from 'react-select'
+
+const options = [
+  { 
+    value: 'caseOne',
+    label: 'XL'
+  }, {
+    value: 'caseTwo',
+    label: '2XL'
+  }, {
+    value: 'caseThree',
+    label: '4L'
+  }, {
+    value: 'caseFour',
+    label: 'XL/2L'
+  }, {
+    value: 'caseFive',
+    label: 'XL/L/2SM'
+  }
+]
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selectedOption: null
+    }
+  }
+
+  handleChange = (selectedOption) => {
+    console.log('bbbb', selectedOption.value)
+      this.setState({ selectedOption },
+      () => function renderSwitch() { 
+        switch (selectedOption.value) {
+          case 'caseOne':
+            return (
+              <CaseFirst />
+            )
+          case 'caseTwo':
+            return (
+              <CaseSecond />
+            )
+          case 'caseThree':
+            return (
+              <CaseThird />
+            )
+          case 'caseFour':
+            return (
+              <CaseFourth />
+            )
+          case 'caseFive':
+            return (
+              <CaseFifth />
+            )
+        }
+      })
+    console.log('selectedOption.value', selectedOption.value)
+  }
+
   render () {
+    const { selectedOption } = this.state
     return (
       <div className='App'>
-        <CaseFirst />
-        <CaseSecond />
-        <CaseThird />
-        <CaseFourth />
-        <CaseFifth />
+        <Select
+          value={selectedOption}
+          onChange={this.handleChange}
+          options={options}
+        />
+        <div>
+          { selectedOption !== null? () => this.renderSwitch(): null }
+        </div>
       </div>
     )
   }
